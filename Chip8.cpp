aa7494ef2601c5faa2e4457efc65b064f6289b73
BudzioT <Budzio.zsm1@gmail.com>
2024-06-04 21:42:38 +0200

    Fixed typo, added basic files

---
+++ Chip8.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bartosz Budnik on 04.06.2024.
+//
+
+#include "Chip8.h"
---
+++ Chip8.h
@@ -0,0 +1,14 @@
+//
+// Created by Bartosz Budnik on 04.06.2024.
+//
+
+#ifndef CHIP8_EMULATOR_CHIP8_H
+#define CHIP8_EMULATOR_CHIP8_H
+
+
+class Chip8 {
+    
+};
+
+
+#endif //CHIP8_EMULATOR_CHIP8_H
