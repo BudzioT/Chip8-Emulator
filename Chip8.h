654cb150ba919f1900256cbafaa7f40f5beb3924
BudzioT <Budzio.zsm1@gmail.com>
2024-06-04 22:32:10 +0200

    Added loading ROMS

--- Chip8.h
+++ Chip8.h
@@ -5,9 +5,25 @@
 #ifndef CHIP8_EMULATOR_CHIP8_H
 #define CHIP8_EMULATOR_CHIP8_H
 
+#include <cstdint>
 
-class Chip8 {
-    
+class Chip8
+{
+public:
+    void LoadROM(const char* fileName);
+
+public:
+    uint8_t memory[4096];
+    uint8_t registers[16];
+    uint16_t stack[16];
+    uint16_t index;
+    uint16_t pc;
+    uint8_t sp;
+    uint8_t delayTimer;
+    uint8_t soundTimer;
+    uint32_t video[64 * 32];
+    uint8_t keys[16];
+    uint16_t opcode;
 };
 
 
