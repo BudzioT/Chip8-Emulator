bfee2eec0e79e3f3f51d6dc5a81b23c546144227
BudzioT <Budzio.zsm1@gmail.com>
2024-06-05 17:55:44 +0200

    Added opcode functions, implemented a few of them

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -5,6 +5,7 @@
 #include "includes/Chip8.h"
 #include <fstream>
 #include <memory>
+#include <cstring>
 
 /* Starting point of ROM in memory */
 const unsigned int START_MEMORY = 0x200;
@@ -13,8 +14,9 @@ const unsigned int START_FONT_ADDRESS = 0x50;
 /* Size of font in bytes */
 const unsigned int FONT_SIZE = 80;
 
-/* Initialize pc to ROM location */
-Chip8::Chip8() : pc(START_MEMORY)
+/* Initialize Chip8 emulator */
+Chip8::Chip8() : pc(START_MEMORY), randEng(std::chrono::system_clock::now().time_since_epoch().count()),
+    rand(std::uniform_int_distribution<uint8_t>(0, 255))
 {
     /* Open the font file as binary */
     std::ifstream file("../extras/font.bin", std::ios::binary);
@@ -27,7 +29,7 @@ Chip8::Chip8() : pc(START_MEMORY)
         printf("Error: Couldn't get the font!");
         exit(1);
     }
-    
+
     file.close();
 }
 
@@ -44,3 +46,150 @@ void Chip8::LoadROM(const char *fileName) {
         file.read(reinterpret_cast<char*>(&memory[START_MEMORY]), size);
     }
 }
+
+/* Clear the screen */
+void Chip8::OP_00E0() {
+    /* Set all video bits to 0 */
+    memset(video, 0, sizeof(video));
+}
+
+/* Return from subroutine */
+void Chip8::OP_00EE() {
+    pc = stack[--sp];
+}
+
+/* Jump to the given address */
+void Chip8::OP_1NNN() {
+    pc = opcode & 0x0FFF;
+}
+
+/* Call the subroutine at the given address */
+void Chip8::OP_2NNN() {
+    stack[sp++] = pc;
+    pc = opcode & 0xFFF;
+}
+
+/* Skip the next instruction if register VX is equal to the given NN byte */
+void Chip8::OP_3XNN() {
+    if (registers[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
+        pc += 2;
+}
+
+/* Skip the next instruction if register VX isn't equal to the given NN byte  */
+void Chip8::OP_4XNN() {
+    if (registers[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
+        pc += 2;
+}
+
+void Chip8::OP_5XY0() {
+
+}
+
+void Chip8::OP_6XNN() {
+
+}
+
+void Chip8::OP_7XNN() {
+
+}
+
+void Chip8::OP_8XY0() {
+
+}
+
+void Chip8::OP_8XY1() {
+
+}
+
+void Chip8::OP_8XY2() {
+
+}
+
+void Chip8::OP_8XY3() {
+
+}
+
+void Chip8::OP_8XY4() {
+
+}
+
+void Chip8::OP_8XY5() {
+
+}
+
+void Chip8::OP_8XY6() {
+
+}
+
+void Chip8::OP_8XY7() {
+
+}
+
+void Chip8::OP_8XYE() {
+
+}
+
+void Chip8::OP_9XY0() {
+
+}
+
+void Chip8::OP_ANNN() {
+
+}
+
+void Chip8::OP_BNNN() {
+
+}
+
+void Chip8::OP_CXNN() {
+
+}
+
+void Chip8::OP_DXYN() {
+
+}
+
+void Chip8::OP_EX9E() {
+
+}
+
+void Chip8::OP_EXA1() {
+
+}
+
+void Chip8::OP_FX07() {
+
+}
+
+void Chip8::OP_FX0A() {
+
+}
+
+void Chip8::OP_FX15() {
+
+}
+
+void Chip8::OP_FX18() {
+
+}
+
+void Chip8::OP_FX1E() {
+
+}
+
+void Chip8::OP_FX29() {
+
+}
+
+void Chip8::OP_FX33() {
+
+}
+
+void Chip8::OP_FX55() {
+
+}
+
+void Chip8::OP_FX65() {
+
+}
+
--- src/includes/Chip8.h
+++ src/includes/Chip8.h
@@ -6,6 +6,8 @@
 #define CHIP8_EMULATOR_CHIP8_H
 
 #include <cstdint>
+#include <chrono>
+#include <random>
 
 class Chip8
 {
@@ -13,6 +15,42 @@ public:
     Chip8();
 
     void LoadROM(const char* fileName);
+
+private:
+    void OP_00E0();
+    void OP_00EE();
+    void OP_1NNN();
+    void OP_2NNN();
+    void OP_3XNN();
+    void OP_4XNN();
+    void OP_5XY0();
+    void OP_6XNN();
+    void OP_7XNN();
+    void OP_8XY0();
+    void OP_8XY1();
+    void OP_8XY2();
+    void OP_8XY3();
+    void OP_8XY4();
+    void OP_8XY5();
+    void OP_8XY6();
+    void OP_8XY7();
+    void OP_8XYE();
+    void OP_9XY0();
+    void OP_ANNN();
+    void OP_BNNN();
+    void OP_CXNN();
+    void OP_DXYN();
+    void OP_EX9E();
+    void OP_EXA1();
+    void OP_FX07();
+    void OP_FX0A();
+    void OP_FX15();
+    void OP_FX18();
+    void OP_FX1E();
+    void OP_FX29();
+    void OP_FX33();
+    void OP_FX55();
+    void OP_FX65();
 
 public:
     uint8_t memory[4096] { };
@@ -26,6 +64,10 @@ public:
     uint32_t video[64 * 32] { };
     uint8_t keys[16] { };
     uint16_t opcode = 0;
+
+private:
+    std::default_random_engine randEng;
+    std::uniform_int_distribution<uint8_t> rand;
 };
 
 
